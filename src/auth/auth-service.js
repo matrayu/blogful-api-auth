8411302f4920932f315ceaa4b405f7ba4008f59c
matrayu <northridge614@gmail.com>
2019-06-22 16:10:10 -0700

    Update auth-service.js

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,4 +1,6 @@
-const bcrypt = ('bcryptjs')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
 
 const AuthService = {
     getUserWithUserName(db, user_name) {
@@ -9,6 +11,19 @@ const AuthService = {
     
     comparePasswords(password, hash) {
         return bcrypt.compare(password, hash)
+    },
+
+    createJwt(subject, payload) {
+        return jwt.sign(payload, config.JWT_SECRET, {
+            subject,
+            algorithm: 'HS256'
+        })
+    },
+
+    verifyJwt(token) {
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256'],
+        })
     },
 
     parseBasicToken(token) {
