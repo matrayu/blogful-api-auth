f18c90f321e0b9808fd2c6d8e9cc97bb119b7f45
matrayu <northridge614@gmail.com>
2019-06-24 06:48:47 -0700

    Update users-router.js
    Added password hashing

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -28,23 +28,26 @@ usersRouter
             if (hasUserWithUserName)
                 return res.status(400).json({ error: `Username already taken`})
 
-            const newUser = {
-                user_name,
-                password,
-                full_name,
-                nickname,
-                date_created: 'now()',
-            }
+            return UserService.hashPassword(password)
+                .then(hashedPassword => {
+                    const newUser = {
+                        user_name,
+                        password: hashedPassword,
+                        full_name,
+                        nickname,
+                        date_created: 'now()',
+                    }
 
-            return UserService.insertUser(
-                req.app.get('db'),
-                newUser
-            )
-                .then(user => {
-                    res
-                        .status(201)
-                        .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                        .json(UserService.serializeUser(user))
+                    return UserService.insertUser(
+                        req.app.get('db'),
+                        newUser
+                    )
+                        .then(user => {
+                            res
+                                .status(201)
+                                .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                                .json(UserService.serializeUser(user))
+                        })
                 })
         })
         .catch(next)
